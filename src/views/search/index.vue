7962edb2a5e3e16a525fe43b712c85852a52889e
Zenobia <lan13ji@163.com>
2019-10-16 14:57:02 +0800

    搜索-联想建议数据请求并渲染

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -6,29 +6,42 @@
     placeholder="请输入搜索关键词"
     show-icon
     shape="round"
-    @search="onSearch">
+    @search="onSearch" @input="onSearchInput">
     <div slot="action" @click="onSearch">搜索</div>
   </van-search>
 
   <!-- 联想建议 -->
   <van-cell-group>
-    <van-cell title="单元格" icon="search" />
-    <van-cell title="单元格" icon="search" />
+    <van-cell
+      v-for="(item,index) in searchSuggestions"
+      :title="item"
+      :key="index"
+      icon="search" />
   </van-cell-group>
 </div>
 </template>
 
 <script>
+import { getSearchSugstions } from '@/api/search'
 export default {
   name: 'searchIndex',
   data () {
     return {
-      searchText: ''
+      searchText: '',
+      searchSuggestions: [] // 联想建议列表
     }
   },
   methods: {
     onSearch () {
       console.log('onSearch')
+    },
+    async onSearchInput () {
+      const searchText = this.searchText.trim()
+      if (!searchText) return
+      const { data } = await getSearchSugstions({
+        q: this.searchText
+      })
+      this.searchSuggestions = data.data.options
     }
   }
 }
