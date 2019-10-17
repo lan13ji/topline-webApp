89c99a46992c73bb3204083a8fc6a6488df5e3a2
Zenobia <lan13ji@163.com>
2019-10-17 15:33:29 +0800

    文章详情-页面布局及调整样式

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -12,7 +12,7 @@
     <van-loading class="article-loading" />
 
     <!-- 文章详情 -->
-    <!-- <div class="detail">
+    <div class="detail">
       <h3 class="title"></h3>
       <div class="author">
         <van-image round width="2rem" height="2rem" fit="fill" src=""></van-image>
@@ -23,8 +23,37 @@
         <van-button round size="small" type="info">+ 关注</van-button>
       </div>
       <div class="content"></div>
-      <div class="zan"></div>
-    </div> -->
+      <div class="zan">
+        <!--
+          round: 圆形按钮
+          hairline: 0.5px细边框
+          plain: 朴素按钮-文字为按钮颜色
+         -->
+        <van-button
+          round
+          hairline
+          plain
+          size="small"
+          type="primary"
+          icon="good-job-o">
+          点赞
+          </van-button>
+          &emsp;
+          <van-button
+          round
+          hairline
+          plain
+          size="small"
+          type="danger"
+          icon="delete">
+          不喜欢
+          </van-button>
+      </div>
+    </div>
+    <!-- 加载失败的消息提示 -->
+    <div class="error">
+      <p>网络超时，点击 <a href="#">刷新</a>试一试</p>
+    </div>
   </div>
 </template>
 
@@ -34,6 +63,62 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.article-container {
+  position: absolute;
+  left: 0;
+  top: 0;
+  overflow-y: scroll;
+  width: 100%;
+  height: 100%;
+}
+.article-loading,
+.error
+{
+  padding-top: 100px;
+  text-align: center;
+}
+.detail {
+  padding: 50px 10px;
 
+  .title {
+    font-size: 16px;
+  }
+
+  .zan {
+    text-align: center
+  }
+
+  .author {
+    padding: 10px 0;
+    display: flex;
+
+    .text {
+      flex: 1;
+      padding-left: 10px;
+      line-height: 1.3;
+
+      .name{
+        font-size: 14px;
+        margin: 0;
+      }
+
+      .time {
+        margin: 0;
+        font-size: 12px;
+        color: #999
+      }
+    }
+  }
+
+  .content {
+    overflow: hidden;
+    white-space: pre-wrap;
+    word-break: break-all;
+    /deep/ img {
+      max-width: 100%;
+      background: #f9f9f9
+    }
+  }
+}
 </style>
